f46b94649ee05471e0bd0117d804e6a668588eda
trancongtien <tientc.21it@vku.udn.vn>
2023-07-21 22:51:15 +0700

    sfarea

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,75 +12,83 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text(" App Bar in Scaffold")),
-      backgroundColor: Colors.blueGrey.shade200,
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
-      body: const Center(child: Text("Body in Scaffold")),
-      // extendBody: ,
-      primary: true,
-      endDrawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.amber));
+    return SafeArea(
+      bottom: true,
+      top: true,
+      left: false,
+      right: false,
+       
+      child: Scaffold(
+        appBar: AppBar(title: const Text(" App Bar in Scaffold")),
+        backgroundColor: Colors.blueGrey.shade200,
+        floatingActionButton:
+            FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+        body: const Center(child: Text("Body in Scaffold")),
+        // extendBody: ,
+        primary: true,
+        endDrawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('End Drawer  '),
               ),
-              child: Text('End Drawer  '),
+            ],
+          ),
+        ),
+        onEndDrawerChanged: (isOpened) {
+          print("$isOpened" + "tien");
+        },
+        bottomSheet: Container(
+            color: Colors.amber,
+            padding: const EdgeInsets.all(12),
+            child: const Text("Bottom Sheet in Scaffold")),
+        drawerDragStartBehavior: DragStartBehavior.down,
+        persistentFooterButtons: <Widget>[
+          ElevatedButton(
+            onPressed: () {},
+            child: const Align(
+              alignment: Alignment.center,
+              child: Text('Footer'),
             ),
-          ],
-        ),
-      ),
-      onEndDrawerChanged: (isOpened) {
-        print("$isOpened" + "tien");
-      },
-      bottomSheet: Container(
-          color: Colors.amber,
-          padding: const EdgeInsets.all(12),
-          child: const Text("Bottom Sheet in Scaffold")),
-      drawerDragStartBehavior: DragStartBehavior.down,
-      persistentFooterButtons: <Widget>[
-        ElevatedButton(
-          onPressed: () {},
-          child: const Align(
-            alignment: Alignment.center,
-            child: Text('Footer'),
-          ),
-        ),
-      ],
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer  '),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
           ),
         ],
-        currentIndex: 1,
-        selectedItemColor: Colors.amber[800],
-        onTap: (int) {},
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer  '),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+          currentIndex: 1,
+          selectedItemColor: Colors.amber[800],
+          onTap: (int) {},
+        ),
       ),
     );
   }
